64635e945ade3bce290b0203a20cacce2b07da18
= <=>
2023-02-25 13:34:53 +0530

    ver003 initial push

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="j2config",
-    version="0.0.2",
+    version="0.0.3",
     author="ALIASGAR - ALI",
     author_email="aholo2000@gmail.com",
     description="configuration generation using jinja2 - for Networking Geeks",
